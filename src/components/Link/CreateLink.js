1cbfc2c4ca636d453d967177749faacca361edb2
jmshanahan <comeraghsolutions@gail.com>
2020-02-10 19:05:24 +0000

    Added link to firestore

--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import useFormValidation from "../Auth/useFormValidation";
 import validateCreateLink from "../Auth/validateCreateLink";
+import { FirebaseContext } from "../../firebase";
 const INITIAL_STATE = {
   description: "",
   url: ""
 };
 
 function CreateLink(props) {
+  const { firebase, user } = useContext(FirebaseContext);
+
   const { handleSubmit, handleChange, values, errors } = useFormValidation(
     INITIAL_STATE,
     validateCreateLink,
     handleCreateLink
   );
   function handleCreateLink() {
+    if (!user) {
+      props.history.push("/login");
+    } else {
+      const { url, description } = values;
+      const newLink = {
+        url,
+        description,
+        postedBy: {
+          id: user.uid,
+          name: user.displayName
+        },
+        votes: [],
+        comments: [],
+        created: Date.now()
+      };
+      firebase.db.collection("links").add(newLink);
+      props.history.push("/");
+    }
     console.log("link created");
   }
   return (
@@ -29,7 +50,7 @@ function CreateLink(props) {
       {errors.description && <p className="error-text">{errors.description}</p>}
       <input
         name="url"
-        type="text"
+        type="url"
         placeholder="A url for the link"
         autoComplete="off"
         onChange={handleChange}
