656f21b32f42c83a3085036931fe65663991d747
jmshanahan <comeraghsolutions@gail.com>
2020-02-11 12:35:44 +0000

    Displaying links completed

--- src/components/Link/LinkItem.js
+++ src/components/Link/LinkItem.js
@@ -1,7 +1,34 @@
 import React from "react";
-
-function LinkItem() {
-  return <div>LinkItem</div>;
+import { Link } from "react-router-dom";
+import distanceInWordsToNow from "date-fns/difference_in_hours";
+import { getDayOfYear, differenceInHours } from "date-fns";
+import { getDomain } from "../../utils";
+function LinkItem({ link, index, showCount }) {
+  console.log({ link });
+  return (
+    <div className="flex items-start mt2">
+      <div className="flex items-center">
+        {showCount && <span className="grey">{index}</span>}
+        <div className="vote-button">{"\u2b06"}</div>
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description}
+          <span className="link">{getDomain(link.url)}</span>
+        </div>
+        <div className="f6 lh-copy grey">
+          {link.votes.length} votes by {link.postedBy.name}{" "}
+          {differenceInHours(Date.now(), link.created)} {" | "}
+          <Link to={`/link/${link.id}`}>
+            {link.comments.length > 0
+              ? `${link.comments.length} comments`
+              : "discuss"}
+          </Link>
+        </div>
+      </div>
+      LinkItem
+    </div>
+  );
 }
 
 export default LinkItem;
--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -1,10 +1,37 @@
-import React from "react";
-// import useAuth from "../Auth/useAuth";
+import React, { useContext, useEffect, useState } from "react";
+import { FirebaseContext } from "../../firebase";
+import LinkItem from "./LinkItem";
 
 function LinkList(props) {
-  // const user = useAuth();
-  // console.log(user);
-  return <div>LinkList</div>;
+  const { firebase } = useContext(FirebaseContext);
+  const [links, setLinks] = useState([]);
+  useEffect(() => {
+    getLinks();
+  }, []);
+
+  function getLinks() {
+    firebase.db.collection("links").onSnapshot(handleSnapshot);
+  }
+
+  function handleSnapshot(snapshot) {
+    const links = snapshot.docs.map(doc => {
+      return { id: doc.id, ...doc.data() };
+    });
+    setLinks(links);
+  }
+
+  return (
+    <div>
+      {links.map((link, index) => (
+        <LinkItem
+          key={link.id}
+          showCount={true}
+          link={link}
+          index={index + 1}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default LinkList;
